9daba6744134ef99bc0856a33a3e1519acc9af08
Ferdi Ardiansa <ferdiardiansa@gmail.com>
2026-08-05 14:56:40 +0700

    build(web): load SVG files as React components via SVGR

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,6 +3,21 @@ import { withNx } from '@nx/next/plugins/with-nx'
 
 const baseConfig = {
   reactCompiler: true,
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },
+  webpack(config: { module: { rules: unknown[] } }) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
 }
 
 const configWithPlugins = baseConfig
